46c64f5a432fba44ead15eb724d1e2236fce42d0
Joshua Galla <gallaj@umich.edu>
2024-08-23 16:42:25 -0400

    reshaped array for xs, x_star to match data dimensions

--- analysis/config.py
+++ analysis/config.py
@@ -2,7 +2,7 @@ import yaml
 import numpy as np
 import pandas as pd
 
-class Config():
+class config():
     def __init__(self, file):
         self.params = self.read_config(file)
         self.__dict__.update(self.params)
@@ -10,38 +10,42 @@ class Config():
     def read_config(self, file):
         
         """
-        reads in dataset, parameters, define kernel and length, and initialize dimensions of data
+        reads in dataset, parameters, and initializes array dimensions of data
 
         Args:
             file (str): .yml file of parameters ("parameters.yml")
 
         Returns:
-            parameters (dict): dictionary of parameters from parameters.yml file that have been updated
+            None
         """
         
         with open(file, 'r') as file:
             parameters = yaml.safe_load(file)
 
-        ## set parameters from 'General'
+        #set parameters from General
         np.random.seed(parameters['General']['seed'])
-        self.method = parameters['General']['method']
-        
         self.response_data = parameters['General']['response_data'] 
         self.stim_data = parameters['General']['stim_data'] 
+
+        #set parameters from Neurons
+        self.num_neurons = parameters['Neurons']['num_neurons']
         self.neuron = parameters['Neurons']['neuron'] 
 
-        self.kernels = np.array([parameters['Interpolate']['kernel'], [parameters['Interpolate']['kernel']]) 
-
+        #set array for interpolation
         exs = []
+        for i in range(1, 3):
+            exs.append(np.arange(10))
+
         xs = np.meshgrid(*exs)
         x_star = np.empty(xs[0].shape + (2,))
         for i in range(2):
             x_star[...,i] = xs[i]
         self.x_star = x_star.reshape(-1, 2)
         
+        #set parameters from Interpolate
         self.gamma = np.array([parameters['Interpolate']['gamma'], parameters['Interpolate']['gamma']])
         self.var = parameters['Interpolate']['var']                     
         self.nu = float(parameters['Interpolate']['nu'])                
-        self.eta = float(parameters['Interpolate']['eta'])              
-
+        self.eta = float(parameters['Interpolate']['eta'])   
+        
         return parameters
