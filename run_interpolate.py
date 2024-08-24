b0f53e30aa3bf10537dc2ca90b5b750ac26a57f8
JoshuaGalla <joshuagalla18@gmail.com>
2024-08-24 15:58:47 -0400

    commented out tuning curve interp plot

--- run_interpolate.py
+++ run_interpolate.py
@@ -14,8 +14,8 @@ neuron = config.neuron
 x_star, stim_data, response_data, avg_map, param1, param2, neuron = sim_data_analysis().get_sim_data(neuron)
 
 f, sigma = fit_gp().calc_interp(stim_data, response_data, x_star, config.var, config.gamma, config.eta)
-print(f)
-print(sigma)
+#print(f)
+#print(sigma)
 
 with open('./data/interp_data_f.npy', 'wb') as file:
     np.save(file,f)
@@ -23,6 +23,6 @@ with open('./data/interp_data_sigma.npy', 'wb') as file:
     np.save(file,sigma)
 
 sim_data_plots().plot_tc(avg_map, param1, param2, neuron)
-sim_data_plots().plot_tc_interp(param1, param2, neuron)
+#sim_data_plots().plot_tc_interp(param1, param2, neuron)
 
 sys.exit()
